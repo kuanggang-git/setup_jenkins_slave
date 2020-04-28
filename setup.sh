3e6d7adfb8e489985eb74bbda8091e9e5b4f80ab
kuanggang-git <62918167+kuanggang-git@users.noreply.github.com>
2020-04-28 15:52:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ cp jenkins-slave.init /etc/init.d/jenkins-slave
 chmod 770 /etc/init.d/jenkins-slave
 chkconfig --add jenkins-slave
 chkconfig --level 3 jenkins-slave on
+sleep 5s
 systemctl restart jenkins-slave
 chmod 644 monitor-jenkins
 cp monitor-jenkins /etc/cron.d/
