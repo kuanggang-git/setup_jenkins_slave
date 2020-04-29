ec016957146ec20ade41a5200476a1cb9363108b
kuanggang-git <62918167+kuanggang-git@users.noreply.github.com>
2020-04-29 15:40:07 +0800

    Update jenkins-monitor.sh

--- jenkins-monitor.sh
+++ jenkins-monitor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:~/bin
 export PATH
-c=$(systemctl status jenkins-slave | grep "java -jar agent.jar"|grep -v grep | wc -l)
+c=$(ps -ef | grep "java -jar agent.jar"|grep -v grep | wc -l)
 if [ $c -eq 0 ];then
   systemctl stop jenkins-slave
   sleep 2s
